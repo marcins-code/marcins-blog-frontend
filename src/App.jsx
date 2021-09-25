dec7967616b36849a79766c815c540755bdac228
marcin's-code <mpaczkowski40@gmail.com>
2021-09-25 06:30:23 +0200

    Testc lints stage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GlobalTemplate from './templates/GlobalTemplate';
-import Homepage from './views/pages/Homepage';
-import Categories from './views/pages/Categories';
-import Series from './views/pages/Series';
-import Contact from './views/pages/Contact';
-import Glossary from './views/pages/Glossary';
-import AdminArticles from './views/admin/AdminArticles';
-import AdminArticleTypes from './views/admin/AdminArticleTypes';
-import AdminGlossary from './views/admin/AdminGlossary';
-import Login from './views/admin/Login';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GlobalTemplate from "./templates/GlobalTemplate";
+import Homepage from "./views/pages/Homepage";
+import Categories from "./views/pages/Categories";
+import Series from "./views/pages/Series";
+import Contact from "./views/pages/Contact";
+import Glossary from "./views/pages/Glossary";
+import AdminArticles from "./views/admin/AdminArticles";
+import AdminArticleTypes from "./views/admin/AdminArticleTypes";
+import AdminGlossary from "./views/admin/AdminGlossary";
+import Login from "./views/admin/Login";
 
 const App = () => (
   <BrowserRouter>
@@ -21,22 +21,24 @@ const App = () => (
         <Route path="/contact" exact component={Contact} />
         <Route path="/glossary" exact component={Glossary} />
         <Route path="/administration" exact component={Login} />
-        <Route path="/administration/articles" exact component={AdminArticles} />
-        <Route path="/administration/article-types" exact component={AdminArticleTypes} />
-        <Route path="/administration/glossary" exact component={AdminGlossary} />
+        <Route
+          path="/administration/articles"
+          exact
+          component={AdminArticles}
+        />
+        <Route
+          path="/administration/article-types"
+          exact
+          component={AdminArticleTypes}
+        />
+        <Route
+          path="/administration/glossary"
+          exact
+          component={AdminGlossary}
+        />
       </GlobalTemplate>
     </Switch>
   </BrowserRouter>
 );
-
-
-
-
-
-
-
-
-
-
 
 export default App;
