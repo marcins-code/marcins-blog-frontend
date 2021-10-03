f35cdd7ed379d017d295cacfc4c7e4da8adf029b
marcin's-code <mpaczkowski40@gmail.com>
2021-10-03 17:16:20 +0200

    Add authorization context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { AuthContext } from './context';
 import GlobalTemplate from './templates/GlobalTemplate';
 import Homepage from './views/pages/Homepage';
 import Categories from './views/pages/Categories';
@@ -10,23 +11,63 @@ import AdminArticles from './views/admin/AdminArticles';
 import AdminArticleTypes from './views/admin/AdminArticleTypes';
 import AdminGlossary from './views/admin/AdminGlossary';
 import Login from './views/admin/Login';
+import { useAuth } from './hooks/useAuth';
+// import NotFound from './views/error/404-page';
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <GlobalTemplate>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/categories" exact component={Categories} />
-        <Route path="/series" exact component={Series} />
-        <Route path="/contact" exact component={Contact} />
-        <Route path="/glossary" exact component={Glossary} />
-        <Route path="/administration" exact component={Login} />
-        <Route path="/administration/articles" exact component={AdminArticles} />
-        <Route path="/administration/article-types" exact component={AdminArticleTypes} />
-        <Route path="/administration/glossary" exact component={AdminGlossary} />
-      </GlobalTemplate>
-    </Switch>
-  </BrowserRouter>
-);
+const App = () => {
+  const authHandler = useAuth();
+  const {
+    isLoggedIn,
+    userId,
+    userRoles,
+    userFirstName,
+    userLastName,
+    token,
+    tokenExpiration,
+    userAvatar,
+    login,
+    logout,
+  } = authHandler;
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        userId,
+        userRoles,
+        userFirstName,
+        userLastName,
+        token,
+        tokenExpiration,
+        userAvatar,
+        login,
+        logout,
+      }}
+    >
+      <BrowserRouter>
+        <Switch>
+          <GlobalTemplate>
+            <Route path="/" exact component={Homepage} />
+            <Route path="/categories" exact component={Categories} />
+            <Route path="/series" exact component={Series} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/glossary" exact component={Glossary} />
+            <Route path="/administration" exact component={Login} />
+
+            <Route path="/administration/articles" exact component={AdminArticles} />
+            <Route path="/administration/article-types">
+              {!authHandler.isLoggedIn ? <Redirect to="/administration" /> : <AdminArticleTypes />}
+            </Route>
+            <Route path="/administration/glossary">
+              {!authHandler.isLoggedIn ? <Redirect to="/administration" /> : <AdminGlossary />}
+            </Route>
+            {/* <Route path="/administration/glossary" exact component={AdminGlossary} /> */}
+            {/* <Redirect from="*" to="/" /> */}
+          </GlobalTemplate>
+        </Switch>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
