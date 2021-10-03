82bc83219fb916fe34ef9977ee110e4a795e1771
marcin's-code <mpaczkowski40@gmail.com>
2021-10-03 17:28:37 +0200

    Refactor by eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { AuthContext } from './context';
