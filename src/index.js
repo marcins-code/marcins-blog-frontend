484b2ae810f0450ec3d68b1667a9c1fdb8c1a5e7
marcin's-code <mpaczkowski40@gmail.com>
2021-10-03 17:17:15 +0200

    Add axios  defaults.baseURL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import App from './App';
 import './assets/css/icomoon.css';
+
+axios.defaults.baseURL = process.env.REACT_APP_BAR_AXIOS_BASE_URL;
 
 ReactDOM.render(
   <React.StrictMode>
