ab1969082462d2e60c6cef0bc444c9ad7dbdf73b
marcin's-code <mpaczkowski40@gmail.com>
2021-10-08 12:59:22 +0200

    Refactor test

--- src/__test__/components/atoms/Backdrop/Backdrop.spec.jsx
+++ src/__test__/components/atoms/Backdrop/Backdrop.spec.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { render, screen, waitFor, cleanup } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Backdrop from '../../../../components/atoms/Backdrop/Backdrop';
 
-afterEach(cleanup);
-
 describe('Backdrop tests', () => {
   test('Should not be on screen with backdropShow false', () => {
-    render(<Backdrop backdropShow={false} onClick={() => {}} />);
-    const backdrop = screen.queryByTestId('backdrop');
-    expect(backdrop).not.toBeInTheDocument();
+    act(() => {
+      render(<Backdrop backdropShow={false} onClick={() => {}} />);
+    });
+    expect(screen.queryByTestId('backdrop')).not.toBeInTheDocument();
   });
 
   test('Should be on screen with backdropShow true', async () => {
-    render(<Backdrop backdropShow onClick={() => {}} />);
-    const backdrop = screen.queryByTestId('backdrop');
-    await waitFor(() => {
-      expect(backdrop).toBeInTheDocument();
+    act(() => {
+      render(<Backdrop backdropShow onClick={() => {}} />);
     });
+    await waitFor(() => {
+      expect(screen.queryByTestId('backdrop')).toBeInTheDocument();
+    });
+  });
+
+  test('Should be on on click function called once', async () => {
+    const mockOnClick = jest.fn();
+    act(() => {
+      render(<Backdrop backdropShow onClick={mockOnClick} />);
+    });
+    expect(mockOnClick).toBeCalledTimes(0);
+
+    act(() => {
+      screen.queryByTestId('backdrop').dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    });
+    expect(mockOnClick).toBeCalledTimes(1);
   });
 });
