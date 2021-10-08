86c3d416fd9a58984f5ef18de218efdc2872dc7f
marcin's-code <mpaczkowski40@gmail.com>
2021-10-08 13:09:09 +0200

    Refactor code

--- src/components/atoms/Heading/Heading.jsx
+++ src/components/atoms/Heading/Heading.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Heading = ({ headingSize, headingContent }) =>
-  (headingSize === 'biggest' && <h1>{headingContent}</h1>) ||
-  (headingSize === 'big' && <h2>{headingContent}</h2>) ||
-  (headingSize === 'medium' && <h3>{headingContent}</h3>) ||
-  (headingSize === 'small' && <h4>{headingContent}</h4>) ||
-  (headingSize === 'very-small' && <h5>{headingContent}</h5>) ||
-  (headingSize === 'smallest' && <h6>{headingContent}</h6>);
+const Heading = ({ headingSize, headingContent }) => (headingSize === 'biggest' && <h1>{headingContent}</h1>)
+  || (headingSize === 'big' && <h2>{headingContent}</h2>)
+  || (headingSize === 'medium' && <h3>{headingContent}</h3>)
+  || (headingSize === 'small' && <h4>{headingContent}</h4>)
+  || (headingSize === 'very-small' && <h5>{headingContent}</h5>)
+  || (headingSize === 'smallest' && <h6>{headingContent}</h6>);
 Heading.propTypes = {
   headingSize: PropTypes.oneOf(['biggest', 'big', 'medium', 'small', 'very-small', 'smallest'])
     .isRequired,
